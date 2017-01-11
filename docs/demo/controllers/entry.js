190fcae01e18ee0a15dab6e6cfb90997312e0f4c
benburton <benjamin.joseph.burton@gmail.com>
2017-01-11 13:38:04 -0500

    Remove unnecessary docs/demo files

--- docs/demo/controllers/entry.js
+++
@@ -1,2 +0,0 @@
-exports['corespring-inline-choice-ng15'] = require('corespring-inline-choice-ng15-controller');
-exports['corespring-inline-choice-ng15'].version =  '../node_modules/corespring-inline-choice-ng15/controller';
